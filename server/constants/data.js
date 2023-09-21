ee82d5fb355b448bace5823be67f1c33a3c8b3be
Akash <akashy0524@gmail.com>
2023-09-21 14:04:56 +0530

    bottom Section updated

--- server/constants/data.js
+++ server/constants/data.js
@@ -1,5 +1,5 @@
 export const bannerData = [
-    { id: 1, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/d117a62eb5fbb8e1.jpg?q=50' },
+    { id: 1, url: 'https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/fd86cf067aff745c.jpg?q=20' },
     { id: 2, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/57267a180af306fe.jpg?q=50' },
     { id: 3, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/ae9966569097a8b7.jpg?q=50' },
     { id: 4, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/f6202f13b6f89b03.jpg?q=50' }
